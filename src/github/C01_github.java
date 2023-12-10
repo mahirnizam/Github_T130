df7034f416339ceaae80289640338629160c8414
mahirnizam <diamondhavvos@outlook.com>
2023-12-10 13:56:25 +0300

    2.Günİlk version

--- src/github/C01_github.java
+++ src/github/C01_github.java
@@ -1,4 +1,4 @@
-package gitgub;
+package github;
 
 public class C01_github {
     public static void main(String[] args) {
---
+++ src/github/C02_github.java
@@ -0,0 +1,10 @@
+package github;
+
+public class C02_github {
+    public static void main(String[] args) {
+        int sayi1 = 10;
+        int sayi2 = 20;
+
+        System.out.println("İki sayının toplamı : " + (sayi1 + sayi2));
+    }
+}
